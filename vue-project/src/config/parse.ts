b8a03598e5f4cb0a8b41ef0c4fdae347df8bd2b7
AnteVol <ante.volarevic@fer.hr>
2025-01-20 23:52:09 +0100

    Added export class

--- vue-project/src/config/parse.ts
+++ vue-project/src/config/parse.ts
@@ -1,7 +1,9 @@
 import Parse from 'parse';
 
-Parse.initialize(
-  "zFd1aRmpnNOlhvdcbD5u8XE0BMmEfcUhFmVLQfHs", 
-  "siVCGcQ20cQ3I3KXe3kwbRKPsZHiUoRUvlWt3zE8"
-);
-Parse.serverURL = 'https://parseapi.back4app.com/';
+export const initializeParse = () => {
+    Parse.initialize(
+      "zFd1aRmpnNOlhvdcbD5u8XE0BMmEfcUhFmVLQfHs",
+      "siVCGcQ20cQ3I3KXe3kwbRKPsZHiUoRUvlWt3zE8"
+    );
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+  };
